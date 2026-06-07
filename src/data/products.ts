0dac7740f3e648daf924066aff914e4ebd1911d8
Vikitohyah <victoriaedorh@gmail.com>
2026-06-07 21:23:08 +0100

    conflicts resolved

--- src/data/products.ts
+++ src/data/products.ts
@@ -3,44 +3,69 @@ export const products = [
     id: "101",
     sellerId: "1",
     title: "Ceramic Vase",
-    price: "$60",
-    image:
-      "https://images.unsplash.com/photo-1612196808214-b7e239e5f4b4?q=80&w=800",
+    description: "Handmade ceramic vase",
+    price: 60,
+    images: [
+      "https://images.unsplash.com/photo-1578500494198-246f612d3b3d?q=80&w=800"
+    ],
+    stock: 10,
+    averageRating: 4.8,
+    reviewCount: 12
   },
 
   {
     id: "102",
     sellerId: "1",
     title: "Clay Mug",
-    price: "$35",
-    image:
+    description: "Handmade clay mug",
+    price: 35,
+    images: [
       "https://images.unsplash.com/photo-1514228742587-6b1558fcf93a?q=80&w=800",
+    ],
+    stock: 20,
+    averageRating: 4.6,
+    reviewCount: 8
   },
 
   {
     id: "103",
-    sellerId: "1",
+    sellerId: "2",
     title: "Decor Bowl",
-    price: "$48",
-    image:
-      "https://images.unsplash.com/photo-1578500494198-246f612d3b3d?q=80&w=800",
+    description: "Handmade decor bowl",
+    price: 48,
+    images: [
+      "https://images.unsplash.com/photo-1578500494198-246f612d3b3d?q=80&w=800"
+    ],
+    stock: 15,
+    averageRating: 4.7,
+    reviewCount: 10
   },
 
   {
     id: "104",
     sellerId: "2",
     title: "Leather Handbag",
-    price: "$120",
-    image:
-      "https://images.unsplash.com/photo-1584917865442-de89df76afd3?q=80&w=800",
+    description: "Handmade leather handbag",
+    price: 120,
+    images: [
+      "https://images.unsplash.com/photo-1584917865442-de89df76afd3?q=80&w=800"
+    ],
+    stock: 5,
+    averageRating: 4.9,
+    reviewCount: 15
   },
 
   {
     id: "105",
     sellerId: "2",
     title: "Leather Sandals",
-    price: "$75",
-    image:
-      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=800",
+    description: "Handmade leather sandal",
+    price: 75,
+    images: [
+      "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=800"
+    ],
+    stock: 10,
+    averageRating: 4.8,
+    reviewCount: 12
   },
 ];
